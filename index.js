e097bc6012e453b0420f7c483d4d6daf787c6527
your name <sudhakarsinghbaghel123321@gmail.com>
2023-10-30 19:12:18 +0530

    put / in url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '/', 'public')));
 app.get("/", (req, res) => {
 
   res.sendFile(
-    path.join(__dirname, '..', 'public', 'index.html')
+    path.join(__dirname, '/', 'public', 'index.html')
   );
 });
 
